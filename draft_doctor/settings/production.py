a953fbce846746e87e6343ed90934bc0febe81d4
Yuta Nakamura <nakamurayuta0204@gmail.com>
2023-10-01 03:24:14 +0900

    feat: add docker-compose.yml for production build

--- draft_doctor/settings/production.py
+++ draft_doctor/settings/production.py
@@ -4,7 +4,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 DJANGO_VITE_DEV_MODE = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['web']
 
 LOGGING = {
     'version': 1,
