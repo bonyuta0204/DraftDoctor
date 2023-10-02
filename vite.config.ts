23200528ea5b6be0b420ec8c3dca3f322bfb924f
bonyuta0204 <nakamurayuta0204@gmail.com>
2023-10-03 02:21:42 +0900

    feature/add js formatter (#2)
    * chore: add eslint and prettier
    
    * fix: eslint and prettier
    
    * fix: vscode settings
    
    * fix: add missing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // 開発環境のmain.tsxが置いてある場所
-  root: resolve("./frontend/src"),
+  root: resolve('./frontend/src'),
 
   resolve: {
     alias: {
@@ -16,17 +16,17 @@ export default defineConfig({
   },
 
   // Djangoでの静的ファイル配信設定である STATIC_URL と同じになるよう設定
-  base: "/static/",
+  base: '/static/',
 
   build: {
-    outDir: resolve("./static/dist"),
-    assetsDir: "",
+    outDir: resolve('./static/dist'),
+    assetsDir: '',
     manifest: true,
     emptyOutDir: true,
-    target: "es2015",
+    target: 'es2015',
     rollupOptions: {
       input: {
-        main: resolve("./frontend/src/main.tsx"),
+        main: resolve('./frontend/src/main.tsx'),
       },
       output: {
         chunkFileNames: undefined,
@@ -35,7 +35,7 @@ export default defineConfig({
   },
 
   server: {
-    host: "localhost",
+    host: 'localhost',
     port: 5173,
     open: false,
     watch: {
