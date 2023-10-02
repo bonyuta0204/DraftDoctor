be3b0346030146e6252a4dc79bb3eeccfe01139c
Yuta Nakamura <nakamurayuta0204@gmail.com>
2023-10-03 01:51:30 +0900

    chore: add eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    // add your custom rules here
+  },
+};
